4137a9a61f351bfb1fd814f521631119d58f0100
coolRoger <roger_liao@hotmail.com>
2026-05-12 15:54:56 +0800

    feat: add ai chat ui example usage for the package

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,20 +1,84 @@
-import type { Component } from "solid-js";
+import { useChat } from "@kodehort/ai-sdk-solid";
+import { DefaultChatTransport } from "ai";
+import { type Component, createSignal, For } from "solid-js";
 import { StreamdownSolid } from "src";
 import styles from "./App.module.css";
-import logo from "./logo.svg";
 
 const App: Component = () => {
+    const { messages, sendMessage } = useChat({
+        transport: new DefaultChatTransport({
+            api: "http://localhost:3000/api/ai/chat/test",
+        }),
+    });
+
+    const [input, setInput] = createSignal("");
+
     return (
-        <div class={styles.App}>
-            <header class={styles.header}>
-                <h1>
-                    <StreamdownSolid>
-                        {
-                            "# Streamdown SolidJs \n## h2 Heading \n### h3 Heading \n#### h4 Heading \n##### h5 Heading \n###### h6 Heading \nAlternatively, for H1 and H2, an underline-ish style: \nAlt-H1 \n====== \nAlt-H2 \n------ \n"
-                        }
-                    </StreamdownSolid>
-                </h1>
-            </header>
+        <div
+            style={{
+                padding: "20px",
+                width: "100vw",
+                height: "100vh",
+                position: "fixed",
+                top: 0,
+                left: 0,
+                display: "flex",
+                "flex-direction": "column",
+            }}
+        >
+            <div
+                style={{
+                    flex: 1,
+                    height: 0,
+                    overflow: "hidden",
+                    "overflow-x": "hidden",
+                    "overflow-y": "auto",
+                }}
+            >
+                <For each={messages}>
+                    {(message) => (
+                        <div>
+                            <p>{message.role}:</p>
+                            <For each={message.parts}>
+                                {(part) => {
+                                    switch (part.type) {
+                                        case "text": {
+                                            return (
+                                                <StreamdownSolid>
+                                                    {part.text}
+                                                </StreamdownSolid>
+                                            );
+                                        }
+                                        default: {
+                                            return null;
+                                        }
+                                    }
+                                }}
+                            </For>
+                        </div>
+                    )}
+                </For>
+            </div>
+            <div style={{ "margin-top": "auto" }}>
+                <input
+                    type="text"
+                    placeholder="message"
+                    value={input()}
+                    oninput={(e) => {
+                        setInput(e.target.value);
+                    }}
+                />
+                <button
+                    type="button"
+                    onClick={() =>
+                        sendMessage({
+                            text: input(),
+                        })
+                    }
+                >
+                    Send
+                </button>
+            </div>
         </div>
     );
 };
